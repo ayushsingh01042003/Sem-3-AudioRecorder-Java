04fcf3942f5516c02d2ee637594f8463e7195731
Ayush <ayushsingh01042003@gmail.com>
2023-01-30 23:59:28 +0530

    added ImageButton

--- app/src/main/java/com/example/audiorecorder/MainActivity.java
+++ app/src/main/java/com/example/audiorecorder/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.audiorecorder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageButton micButton;
+    private TextView outputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        micButton = findViewById(R.id.micButton);
+        outputText = findViewById(R.id.outputText);
+        micButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Button Pressed", Toast.LENGTH_SHORT).show();
+                outputText.setText("Recording you sweet voice <3");
+            }
+
+        });
+        micButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Button Pressed", Toast.LENGTH_SHORT).show();
+                outputText.setText("Recording Stopped :(");
+            }
+        });
+        
     }
 }
