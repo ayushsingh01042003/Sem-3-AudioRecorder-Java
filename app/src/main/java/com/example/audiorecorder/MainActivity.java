201aed0c9c4bc40cbef31bbe326b5dfd3552a55c
Ayush <ayushsingh01042003@gmail.com>
2023-02-17 00:07:40 +0530

    added list view

--- app/src/main/java/com/example/audiorecorder/MainActivity.java
+++ app/src/main/java/com/example/audiorecorder/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private Button stopButton;
     private Button listRecordingsButton;
     private boolean isRecording = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/audiorecorder/savedRecordings.java
+++ app/src/main/java/com/example/audiorecorder/savedRecordings.java
@@ -6,13 +6,17 @@ import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class savedRecordings extends AppCompatActivity {
-
+    ListView listRecordings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved_recordings);
+        listRecordings = findViewById(R.id.listRecordings);
     }
 
 }
